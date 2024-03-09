53f79fd904cf5f46f52bee4ef884f64b74d38891
Daniel Nunes <danyribeironunes15@gmail.com>
2024-03-09 18:30:37 +0000

    feat: New shiny sidebar with glowing selected item

--- svelte/src/lib/components/SideBar.svelte
+++ svelte/src/lib/components/SideBar.svelte
@@ -1,44 +1,48 @@
 <script lang="ts">
+    import { tweened } from "svelte/motion";
     import { i18n } from "$lib/stores/i18nStore";
     import { page } from "$lib/stores/pageStore";
     import { disable } from "$lib/stores/disableStore";
     import Icon from "./Icon.svelte";
+    import Button from "./Button.svelte";
 
-    export let disabled: boolean = false;
+    const duration = 2000, shine = tweened(0, {
+		duration
+	});
+
+    function shineScroll() {
+        shine.set(200);
+        setTimeout(() => {
+            shine.set(0, { duration: 0 });
+            shineScroll();
+        }, duration);
+    }
+
+    shineScroll();
 </script>
 
 <div class="w-80 px-4 py-2 space-y-4">
-    <button class="account" {disabled} on:click={() => page.set("home")}>
-        <Icon name="account" className="w-14" />
-        <span>{$i18n.t("sidebar.login")}</span>
-    </button>
-    <hr class="h-[3px] bg-slate-900/10 border-0 rounded-full">
+    <Button type="invisible" className="w-full flex items-center justify-start font-semibold space-x-3" disabled={$disable} on:click={() => page.set("login")}>
+        <Icon name="account" className="w-14 text-primary transition-colors" />
+        <p class={$page.current == "login" || $page.current == "account" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors" : ""} style:background-image={$page.current == "login" || $page.current == "account" ? "linear-gradient(to right, #FFA947, rgb(var(--color-account)), #E07800, rgb(var(--color-account)), #FFA947)" : ""} style:background-position="{$shine}%">{$i18n.t("sidebar.login")}</p>
+    </Button>
+    <hr class="w-full h-[3px] bg-foreground/10 border-0 rounded-full">
     <div>
-        <button {disabled} on:click={() => page.set("home")}>
-            <Icon name="home" className="w-8" />
-            <span>{$i18n.t("sidebar.home")}</span>
-        </button>
-        <button {disabled} on:click={() => page.set("send")}>
-            <Icon name="send" className="w-8" />
-            <span>{$i18n.t("sidebar.send")}</span>
-        </button>
-        <button {disabled} on:click={() => page.set("receive")}>
-            <Icon name="receive" className="w-8" />
-            <span>{$i18n.t("sidebar.receive")}</span>
-        </button>
-        <button {disabled} on:click={() => page.set("settings")}>
-            <Icon name="settings" className="w-8" />
-            <span>{$i18n.t("sidebar.settings")}</span>
-        </button>
+        <Button type="invisible" className="w-full p-2 flex items-center justify-start font-semibold space-x-3" disabled={$disable} on:click={() => page.set("home")}>
+            <Icon name="home" className="w-8 {$page.current == "home" ? "text-[rgb(var(--color-home))]" : ""} transition-colors" />
+            <p class={$page.current == "home" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors" : ""} style:background-image={$page.current == "home" ? "linear-gradient(to right, #4493EE, rgb(var(--color-home)), #1160BB, rgb(var(--color-home)), #4493EE)" : ""} style:background-position="{$shine}%">{$i18n.t("sidebar.home")}</p>
+        </Button>
+        <Button type="invisible" className="w-full p-2 flex items-center justify-start font-semibold space-x-3" disabled={$disable} on:click={() => page.set("send")}>
+            <Icon name="send" className="w-8 {$page.current == "send" ? "text-[rgb(var(--color-send))]" : ""} transition-colors" />
+            <p class={$page.current == "send" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors" : ""} style:background-image={$page.current == "send" ? "linear-gradient(to right, #FF7088, rgb(var(--color-send)), #F50029, rgb(var(--color-send)), #FF7088)" : ""} style:background-position="{$shine}%">{$i18n.t("sidebar.send")}</p>
+        </Button>
+        <Button type="invisible" className="w-full p-2 flex items-center justify-start font-semibold space-x-3" disabled={$disable} on:click={() => page.set("receive")}>
+            <Icon name="receive" className="w-8 {$page.current == "receive" ? "text-[rgb(var(--color-receive))]" : ""} transition-colors" />
+            <p class={$page.current == "receive" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors" : ""} style:background-image={$page.current == "receive" ? "linear-gradient(to right, #47F0FF, rgb(var(--color-receive)), #00A8B8, rgb(var(--color-receive)), #47F0FF)" : ""} style:background-position="{$shine}%">{$i18n.t("sidebar.receive")}</p>
+        </Button>
+        <Button type="invisible" className="w-full p-2 flex items-center justify-start font-semibold space-x-3" disabled={$disable} on:click={() => page.set("settings")}>
+            <Icon name="settings" className="w-8 {$page.current == "settings" ? "text-[rgb(var(--color-settings))]" : ""} transition-colors" />
+            <p class={$page.current == "settings" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors" : ""} style:background-image={$page.current == "settings" ? "linear-gradient(to right, #F47B3E, rgb(var(--color-settings)), #D44F0C, rgb(var(--color-settings)), #F47B3E)" : ""} style:background-position="{$shine}%">{$i18n.t("sidebar.settings")}</p>
+        </Button>
     </div>
 </div>
-
-<style lang="postcss">
-    button {
-        @apply w-full p-2 flex items-center justify-start font-semibold space-x-3;
-    }
-
-    .account {
-        @apply p-0;
-    }
-</style>
