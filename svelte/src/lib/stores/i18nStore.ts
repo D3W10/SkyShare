539e3d6e2a8ce5061de504c60ed1915ff65ca5f4
Daniel Nunes <danyribeironunes15@gmail.com>
2024-03-09 15:53:05 +0000

    fix: Selected language on settings is applied on startup

--- svelte/src/lib/stores/i18nStore.ts
+++ svelte/src/lib/stores/i18nStore.ts
@@ -1,11 +1,12 @@
 import i18next from "i18next";
 import { createI18nStore } from "svelte-i18next";
+import { settings } from "./settingsStore";
 
 import en from "../translations/en.json";
 import pt from "../translations/pt.json";
 
 i18next.init<typeof en>({
-    lng: "en",
+    lng: await new Promise((resolve) => settings.subscribe(($settings) => resolve($settings.language))),
     resources: {
         en: { translation: en },
         pt: { translation: pt }
