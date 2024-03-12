e93bb65fc376249543c9f793a9a7b2194097d3c0
Daniel Nunes <danyribeironunes15@gmail.com>
2024-03-12 23:32:24 +0000

    chore: Improve i18n store to support app build

--- svelte/src/lib/stores/i18nStore.ts
+++ svelte/src/lib/stores/i18nStore.ts
@@ -1,19 +1,25 @@
-import i18next from "i18next";
+import i18next, { type i18n as i18nT } from "i18next";
 import { createI18nStore } from "svelte-i18next";
 import { settings } from "./settingsStore";
 
 import en from "../translations/en.json";
 import pt from "../translations/pt.json";
+import type { Writable } from "svelte/store";
 
-i18next.init<typeof en>({
-    lng: await new Promise((resolve) => settings.subscribe(($settings) => resolve($settings.language))),
-    resources: {
-        en: { translation: en },
-        pt: { translation: pt }
-    }
-});
+let i18n: Writable<i18nT>;
 
-const i18n = createI18nStore(i18next);
+(async () => {
+    i18next.init<typeof en>({
+        lng: await new Promise((resolve) => settings.subscribe(($settings) => resolve($settings.language))),
+        resources: {
+            en: { translation: en },
+            pt: { translation: pt }
+        }
+    });
+
+    i18n = createI18nStore(i18next);
+})();
+
 export {
     i18n
 }
