11a7df433f8704f003bd561f376223769f58d80f
Daniel Nunes <danyribeironunes15@gmail.com>
2024-04-08 14:27:56 +0100

    feat: Animate the sidebar profile picture when signing in and out

--- svelte/src/lib/components/SideBar.svelte
+++ svelte/src/lib/components/SideBar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { scale } from "svelte/transition";
+    import { quartIn, quartOut } from "svelte/easing";
     import { i18n } from "$lib/stores/i18nStore";
     import { page } from "$lib/stores/pageStore";
     import { account } from "$lib/stores/accountStore";
@@ -8,11 +10,15 @@
 
 <div class="w-80 px-4 py-2 space-y-4">
     <Button type="invisible" className="w-full flex items-center justify-start font-semibold space-x-3" on:click={() => page.set(!$account ? "login" : "account")}>
-        {#if !$account || !$account.photo}
-            <Icon name="account" className="w-14 text-primary transition-colors" />
-        {:else}
-            <img src={$account.photo} alt="{$account.username} Profile Picture" class="w-14 h-14 rounded-full" />
-        {/if}
+        <div class="w-14 flex justify-center items-center relative aspect-square">
+            {#if !$account || !$account.photo}
+                <div class="w-full h-full" in:scale={{ duration: 1000, delay: 1000, easing: quartOut, opacity: 1 }} out:scale={{ duration: 1000, easing: quartIn, opacity: 1 }}>
+                    <Icon name="account" className="absolute text-primary transition-colors" />
+                </div>
+            {:else}
+                <img src={$account.photo} alt="{$account.username} Profile Picture" class="max-h-14 absolute rounded-full" in:scale={{ duration: 1000, delay: 1000, easing: quartOut, opacity: 1 }} out:scale={{ duration: 1000, easing: quartIn, opacity: 1 }} />
+            {/if}
+        </div>
         <p class={$page.current == "login" || $page.current == "account" ? "text-transparent bg-[length:200%] bg-clip-text transition-colors animate-[shine_2s_linear_infinite]" : ""} style:background-image={$page.current == "login" || $page.current == "account" ? "linear-gradient(to right, #F5933D, rgb(var(--color-account)), #D46A0D, rgb(var(--color-account)), #F5933D)" : ""}>{!$account ? $i18n.t("sidebar.login") : $account.username}</p>
     </Button>
     <hr class="w-full h-[3px] bg-foreground/10 border-0 rounded-full">
