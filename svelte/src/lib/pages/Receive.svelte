119dd0de4825b6c42407703fb72eed566d5dd945
Daniel Nunes <danyribeironunes15@gmail.com>
2024-05-16 14:28:03 +0100

    feat: Add basic receive page

---
+++ svelte/src/lib/pages/Receive.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { fade, fly } from "svelte/transition";
+    import { i18n } from "$lib/stores/i18nStore";
+    import { page } from "$lib/stores/pageStore";
+    import { transition } from "$lib/stores/transitionStore";
+    import Columns from "$lib/components/layout/Columns.svelte";
+</script>
+
+<div class="w-full h-full p-6" in:fade={$transition.pageIn} out:fade={$transition.pageOut}>
+    {#if $page.subPage == 0}
+        <div class="w-full h-full flex flex-col space-y-4" in:fly={$transition.subpageIn} out:fly={$transition.subpageOut}>
+            <h1 class="w-full text-xl font-semibold">{$i18n.t("receive.title")}</h1>
+            <Columns>
+                <div slot="left"></div>
+                <div slot="right"></div>
+            </Columns>
+        </div>
+    {/if}
+</div>
