84d41a16061e70c95b0e67a71303e3bcf16496ff
Daniel Nunes <danyribeironunes15@gmail.com>
2025-02-05 20:14:12 +0000

    refactor: Make necessary changes to run Svelte 5 and TailwindCSS 4

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,18 +1,21 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-    preprocess: preprocess(),
     kit: {
         adapter: adapter({
             pages: "../dist/www",
             assets: "../dist/www"
         }),
         alias: {
+            "$": ".",
+            "$/*": "./*",
             "$electron": "../electron",
             "$electron/*": "../electron/*"
         }
+    },
+    compilerOptions: {
+        runes: true
     }
 };
 
--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -1,6 +1,10 @@
+import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 
-/** @type {import("vite").UserConfig} */
-export default {
-    plugins: [sveltekit()]
-}
+export default defineConfig({
+    plugins: [
+        sveltekit(),
+        tailwindcss()
+    ]
+});
